a8fe3690b5631867418d8faf76183fca49d9c1f8
Harrietc642 <harriet.c642@gmail.com>
2022-10-13 18:25:45 -0500

    Loading the csv files to the Artwork and Inspiration Table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_185433) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_231845) do
   create_table "artworks", force: :cascade do |t|
     t.string "title"
     t.decimal "price"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_185433) do
     t.integer "inspiration_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "genre"
     t.index ["inspiration_id"], name: "index_artworks_on_inspiration_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require "csv"
 
-Inspiration.delete_all
+Artwork.delete_all
+#Inspiration.delete_all
 
-filename = Rails.root.join("db/")
+=begin
+
+inspiration_filename = Rails.root.join("db/Inspiration1.csv")
+puts "Loading Inspirations from the csv file: #{inspiration_filename}"
+
+csv_data_inspiration = File.read(inspiration_filename)
+inspirations = CSV.parse(csv_data_inspiration, headers:true, encoding: "utf-8")
+
+inspirations.each do |i|
+  Inspiration.create(
+    name: i['name'],
+    year: i['year'],
+    nationality: i['nationality'],
+    wikipedia: i['wikipedia'],
+    painting: i['painting']
+  )
+end
+
+=end
+
+
+
+
+
+
+artwork_filename = Rails.root.join("db/Artwork1.csv")
+puts "Loading Artworks from the csv file: #{artwork_filename}"
+
+csv_data_artwork = File.read(artwork_filename)
+artworks = CSV.parse(csv_data_artwork, headers:true, encoding: "utf-8")
+
+artworks.each do | a |
+  inspiration = Inspiration.find_or_create_by(name: a["inspiration_name"])
+  if inspiration && inspiration.valid?
+    #title: a["title"]
+    artwork = inspiration.artworks.create(
+      title: a['title'],
+      price: a['price'],
+      brand: a['brand'],
+      genre: a['genre']
+    )
+
+  else
+    puts "Invalid inspiration #{a['inspiration_name']} for artwork #{a['title']}"
+  end
+end
+puts "Created #{Inspiration.count} inspirations"
