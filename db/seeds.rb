444c1d972a28d85f54b26805eeecb27c4717f33a
Harrietc642 <harriet.c642@gmail.com>
2022-10-15 20:35:53 -0500

    Added Images to pages and added About page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
 require "csv"
 
-#Artwork.delete_all
+Artwork.delete_all
 #Inspiration.delete_all
 
 =begin
-
 inspiration_filename = Rails.root.join("db/Inspiration1.csv")
 puts "Loading Inspirations from the csv file: #{inspiration_filename}"
-
 csv_data_inspiration = File.read(inspiration_filename)
 inspirations = CSV.parse(csv_data_inspiration, headers:true, encoding: "utf-8")
-
 inspirations.each do |i|
   Inspiration.create(
     name: i['name'],
@@ -20,7 +17,6 @@ inspirations.each do |i|
     painting: i['painting']
   )
 end
-
 =end
 
 
@@ -34,7 +30,6 @@ puts "Loading Artworks from the csv file: #{artwork_filename}"
 csv_data_artwork = File.read(artwork_filename)
 artworks = CSV.parse(csv_data_artwork, headers:true, encoding: "utf-8")
 
-
 artworks.each do | a |
   inspiration = Inspiration.find_or_create_by(name: a["inspiration_name"])
   if inspiration && inspiration.valid?
@@ -53,26 +48,3 @@ artworks.each do | a |
   end
 end
 puts "Created #{Inspiration.count} inspirations"
-
-
-=begin
-artworks.each do | a |
-  inspiration = Inspiration.find_or_create_by(name: a["inspiration_name"])
-  if inspiration && inspiration.valid?
-    #title: a["title"]
-    artwork = inspiration.artworks.create(
-      title: a['title'],
-      price: a['price'],
-      brand: a['brand'],
-      genre: a['genre'],
-      inspiration_name: a['inspiration_name'],
-      bitcoin_address: Faker::Blockchain::Bitcoin.address
-    )
-
-  else
-    puts "Invalid inspiration #{a['inspiration_name']} for artwork #{a['title']}"
-  end
-end
-puts "Created #{Inspiration.count} inspirations"
-=end
-
