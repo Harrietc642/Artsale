52d0800d6a3965898823a84b9501c4f7c958aa2a
Harrietc642 <harriet.c642@gmail.com>
2022-11-30 01:12:01 -0600

    Added shopping cart in gallery view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :initialize_session
   before_action :increment_visit_count, only: %i[index show]
-
+  before_action :load_cart
 
 
   def add_to_cart
+    #id = params[:id].to_i
+    #session[:cart] << id unless session[:cart].include?(id)
     session[:cart] << params[:id]
     redirect_to(gallery_index_path, notice: 'Item added!')
   end
@@ -13,6 +15,11 @@ class ApplicationController < ActionController::Base
   def initialize_session
     session[:visit_count] ||= 0
     session[:cart] ||= []
+  end
+
+  def load_cart
+    #@cart = session[:cart]
+    @cart = Artwork.find(session[:cart])
   end
 
   def increment_visit_count
